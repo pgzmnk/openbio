ff5a1ca88c9fd6a9807e4f000faf2828736d5085
pgzmnk <pgzmnk@gmail.com>
2023-07-13 13:22:52 -0700

    Show basic projects catalog.

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import type { GetStaticProps } from "next";
 import Layout from "@/components/Layout";
-import Post, { PostProps } from "@/components/Post";
+import ProjectCardRow, { ProjectCardRowProps } from "@/components/ProjectCardRow";
 import prisma from "@/lib/prisma";
 
 export const getStaticProps: GetStaticProps = async () => {
@@ -20,47 +20,27 @@ export const getStaticProps: GetStaticProps = async () => {
 };
 
 type Props = {
-    feed: PostProps[];
+    projects: ProjectCardRowProps[];
 };
 
-const Blog: React.FC<Props> = (props) => {
-    console.log('!', process.env.NEXTAUTH_URL)
 
+
+const ProjectCatalog: React.FC<ProjectCardRowProps> = (props) => {
     return (
         <>
             <Layout>
                 <div class="container page mx-auto  px-4">
                     <div class="container">
-                        <h1>Create New Project</h1>
+                        <h1>Projects Catalog</h1>
                         <div class="flex flex-col ">
-                            <input type="email" class="form-input px-4 py-3 rounded-full" />
-                            <div class="flex flex-row">
-                                <div>04</div>
-                                <div>02</div>
-                                <div>03</div>
+                            <div>
+                                {props.feed.map((project: ProjectCardRowProps) => (
+                                    <div key={project.id} className="project p-1">
+                                        <ProjectCardRow project={project} />
+                                    </div>
+                                ))}
                             </div>
-                            <div class="flex flex-row">
-                                <div>01</div>
-                                <div>02</div>
-                                <div>03</div>
-                            </div>
-                            <div class="flex flex-row">
-                                <div>01</div>
-                                <div>02</div>
-                                <div>03</div>
-                            </div>
-                            <div>01</div>
-                            <div>02</div>
-                            <div>03</div>
                         </div>
-                        <main>
-                            {props.feed.map((post) => (
-                                <div key={post.id} className="post">
-                                    {/* <Post post={post} /> */}
-                                    {post.geometry}
-                                </div>
-                            ))}
-                        </main>
                     </div>
                 </div>
             </Layout>
@@ -68,4 +48,4 @@ const Blog: React.FC<Props> = (props) => {
     );
 };
 
-export default Blog;
+export default ProjectCatalog;
