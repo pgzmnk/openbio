98ccc23ad0b2983457ae00233c82aee20f213fd5
pgzmnk <pgzmnk@gmail.com>
2023-07-13 12:53:17 -0700

    Create Project Form.

---
+++ src/pages/project/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Form from "@/components/Form"
+
+
+export default function ProjectForm() {
+
+    return (
+        <div class="flex flex-col ">
+            <Form />
+        </div>
+    )
+}
