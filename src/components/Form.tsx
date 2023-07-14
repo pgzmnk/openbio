48044599251679082bba7421fec1270e19b9a662
pgzmnk <pgzmnk@gmail.com>
2023-07-14 09:52:55 -0700

    Cleanup form.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,6 +3,50 @@ import Map from "./Map";
 import { MapContext, MapGeometryContext } from "@/context/context";
 import { FormDataType, FormProps } from "@/interfaces";
 
+const questions = [
+  {
+    text: "What's the prevailing type of grassland on this plot?",
+    type: "select",
+    options: ["Perennial grassland", "Forest", "Crops", "Other"],
+  },
+  {
+    text: "Was the plot converted to permanent grassland in the last 20 years?",
+    type: "boolean",
+  },
+];
+
+function QuestionComponent({ question }) {
+  return (
+    <>
+      <legend className="text-sm font-semibold leading-6 text-gray-900">
+        {question.text}
+      </legend>
+      {question.type === "boolean" && (
+        <input
+          id="comments"
+          name="comments"
+          type="checkbox"
+          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+        />
+      )}
+
+      {question.type === "select" && (
+        <select
+          id="comments"
+          name="comments"
+          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+        >
+          {question.options?.map((option: string) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      )}
+    </>
+  );
+}
+
 export default function Form({ onSubmit }: FormProps) {
   const { mapGeometry } = useContext(MapGeometryContext);
 
@@ -105,77 +149,18 @@ export default function Form({ onSubmit }: FormProps) {
               Land characteristics
             </h2>
             <p className="mt-1 text-sm leading-6 text-gray-600">
-              These approximations will help you calculate the score.
+              These approximations help you calculate the land's exposure to
+              stressors.
             </p>
 
             <div className="mt-10 space-y-10">
               <fieldset>
-                <legend className="text-sm font-semibold leading-6 text-gray-900">
-                  By Email
-                </legend>
                 <div className="mt-6 space-y-6">
                   <div className="relative flex gap-x-3">
-                    <div className="flex h-6 items-center">
-                      <input
-                        id="comments"
-                        name="comments"
-                        type="checkbox"
-                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                      />
-                    </div>
-                    <div className="text-sm leading-6">
-                      <label
-                        htmlFor="comments"
-                        className="font-medium text-gray-900"
-                      >
-                        Comments
-                      </label>
-                      <p className="text-gray-500">
-                        Get notified when someones posts a comment on a posting.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="relative flex gap-x-3">
-                    <div className="flex h-6 items-center">
-                      <input
-                        id="candidates"
-                        name="candidates"
-                        type="checkbox"
-                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                      />
-                    </div>
-                    <div className="text-sm leading-6">
-                      <label
-                        htmlFor="candidates"
-                        className="font-medium text-gray-900"
-                      >
-                        Candidates
-                      </label>
-                      <p className="text-gray-500">
-                        Get notified when a candidate applies for a job.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="relative flex gap-x-3">
-                    <div className="flex h-6 items-center">
-                      <input
-                        id="offers"
-                        name="offers"
-                        type="checkbox"
-                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                      />
-                    </div>
-                    <div className="text-sm leading-6">
-                      <label
-                        htmlFor="offers"
-                        className="font-medium text-gray-900"
-                      >
-                        Offers
-                      </label>
-                      <p className="text-gray-500">
-                        Get notified when a candidate accepts or rejects an
-                        offer.
-                      </p>
+                    <div className="flex flex-col h-6 items-left">
+                      {questions.map((question) => {
+                        return <QuestionComponent question={question} />;
+                      })}
                     </div>
                   </div>
                 </div>
