a3ab69844c435f42a8ac9283b24ae937a093dbe3
pgzmnk <pgzmnk@gmail.com>
2023-07-14 13:10:18 -0700

    Update projects navbar link.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 const navigation = [
   { name: "Dashboard", href: "#", current: true },
   { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
+  { name: "Projects", href: "/projects", current: false },
   { name: "New Project", href: "/project", current: false },
 ];
 
