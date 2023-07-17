bfe945f9be0d47cae00bd12489bb56bddb07b24b
pgzmnk <pgzmnk@gmail.com>
2023-07-17 15:18:13 -0700

    Calculator based on username.

--- src/pages/calculator/index.tsx
+++ src/pages/calculator/index.tsx
@@ -1,22 +1,35 @@
+import { useSession } from "next-auth/react";
+
+
 export default function Calculator() {
-  const username = "testuser";
+  const { data: session } = useSession();
+  const username = session?.user?.email || "default";
   const gradioAppUrl = new URL(process.env.NEXT_PUBLIC_GRADIO_APP_URL || "");
   gradioAppUrl.searchParams.set("username", username);
+  console.log('gradioappurl', gradioAppUrl.toString());
+
 
   return (
-    <div>
+    <div class="flex-vertical h-screen">
       <h1>Bioscore calculator</h1>
-      <script
+      {/* <script
         type="module"
         src="https://gradio.s3-us-west-2.amazonaws.com/3.36.1/gradio.js"
       ></script>
 
       <gradio-app
-        data="{user: 'testuser'}"
-        src={gradioAppUrl.toString()}
+        // src={gradioAppUrl.toString()}
+        src={gradioAppUrl2}
         theme_mode="light"
         control_page_title="true"
-      />
+      /> */}
+
+      <iframe
+        src={gradioAppUrl.toString()}
+        width="100%"
+        height="100%"
+      ></iframe>
+
     </div>
   );
 }
