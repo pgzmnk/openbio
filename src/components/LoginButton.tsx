4f6a6a3d3d432e454fe106fe1d72aeb650b93ed0
pgzmnk <pgzmnk@gmail.com>
2023-07-17 11:24:37 -0700

    Align login button.

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,4 +1,5 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import { Bars3Icon, BellIcon, ArrowRightCircleIcon } from "@heroicons/react/24/outline";
 
 export default function Component() {
   const { data: session } = useSession();
@@ -11,9 +12,23 @@ export default function Component() {
     );
   }
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+    <button
+      type="button"
+      className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+      onClick={() => signIn()}
+    >
+
+      <div class="inline-flex items-center ">
+        <div class="side">
+          <ArrowRightCircleIcon className="h-6 w-6" aria-hidden="true" />
+        </div>
+        <div class="flex p-1">
+          <div>
+            <p>Login</p>
+          </div>
+        </div>
+      </div>
+
+    </button>
   );
 }
